44005548ffa79d86aa1fd6aed54ddaf8b6a85fec
imran <imrankhannn81@gmail.com>
2020-04-18 20:55:45 +0500

    git completed

---
+++ linux/linux/getting started with linux command line/newloop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for filename in file1 file2 file3
+do
+	echo "important stuff" >> $filename
+done
---
+++ linux/linux/getting started with linux command line/theloop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in {0..10..2}
+do 
+echo "we have been through this $i times already"
+done
